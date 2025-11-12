257765f34cfa7e1d8725988b3e1370b44913519c
Oleksii Ponomarenko <55384068+Oleksii996@users.noreply.github.com>
2025-11-12 17:27:01 +0100

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,52 @@
+//#region Задача 3. Конструктор рядків
+
+// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
+
+// Оголоси наступні методи класу:
+// getValue() — повертає поточне значення приватної властивості value.
+// padEnd(str) — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+// padStart(str) — отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
+// padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+
+// Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+
+{
+  class StringBuilder {
+    #value; //приватна!!!
+
+    constructor(value) {
+      this.#value = value;
+    } //конструктор роблю для передачі властивостей для нових обєктів у класі
+
+    getValue() {
+      return this.#value;
+    }
+
+    padStart(str) {
+      return (this.#value = str + this.#value); //просто додаєм спочатку
+    }
+
+    padEnd(str) {
+      return (this.#value = this.#value + str); //просто додаєм в кінці
+    }
+
+    padBoth(str) {
+      return (this.#value = str + this.#value + str); //просто додаєм в з обох боків
+    }
+  }
+
+  const builder = new StringBuilder('.');
+
+  console.log(builder.getValue()); // "."
+
+  builder.padStart('^');
+  console.log(builder.getValue()); // "^."
+
+  builder.padEnd('^');
+  console.log(builder.getValue()); // "^.^"
+
+  builder.padBoth('=');
+  console.log(builder.getValue()); // "=^.^="
+}
+
+//#endregion
