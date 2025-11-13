d46d55afc983490503e29314b2a34f8c9c2ed2e1
Oleksii Ponomarenko <alexponomarenko996@gmail.com>
2025-11-14 00:36:15 +0100

    update code

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,5 @@
+// Завдання 1
+
 //З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
 //Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
@@ -6,10 +8,14 @@
 const items = document.querySelectorAll('.item');
 // беремо всі елементи з класом item
 console.log('Number of categories:', items.length);
+//Number of categories: 3
 
 items.forEach(item => {
-  const title = item.querySelector('h2');
-  const elements = item.querySelectorAll('li');
-  console.log('Category:', title.textContent);
+  //for Each для кожного елемента item
+  const heads = item.querySelector('h2'); //назви категорій беру звідси
+  const elements = item.querySelectorAll('li'); //кількість елементів у категорії беру звідси
+  console.log('Category:', heads.textContent);
+  //метод для для перетворення на рядок назви
   console.log('Elements:', elements.length);
+  //простто кількість елементів у категорії
 });
