2d10a546d9420caaea65d3c611040af47b115d8d
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-11-13 15:02:43 +0100

    updade code task 1

--- js/task-1.js
+++ js/task-1.js
@@ -2,3 +2,14 @@
 
 //Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
 // Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+
+const items = document.querySelectorAll('.item');
+// беремо всі елементи з класом item
+console.log('Number of categories:', items.length);
+
+items.forEach(item => {
+  const title = item.querySelector('h2');
+  const elements = item.querySelectorAll('li');
+  console.log('Category:', title.textContent);
+  console.log('Elements:', elements.length);
+});
